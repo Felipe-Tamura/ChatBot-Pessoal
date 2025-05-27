cc3c31f121550dac6ede07c267f55e7b7ada9aef
Felipe Tamura <felipetamura59@gmail.com>
2025-05-27 14:15:17 -0300

    feat: adiciona classe principal para execução do chatbot

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import entities.Chatbot;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Chatbot chat = new Chatbot();
+		chat.iniciar();
+	}
+
+}
